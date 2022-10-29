c57384b776647391b0a2d4659811f1df78551481
Olof Hellqvist <olof.hellqvist@gmail.com>
2022-10-29 20:54:59 +0200

    Adding code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+// debug run 
+// cargo run -- --interface eth0
+//
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+#[command(name = "syslog_sniffer")]
+#[command(bin_name = "syslog_sniffer")]
+struct Cli {
+    #[arg(short, long, default_value_t=514)]
+    port: usize,
+    #[arg(short, long)]
+    interface: String,
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    println!("Port to sniff: {:?}", args.port);
+    println!("Interface to sniff: {:?}", args.interface);
 }
